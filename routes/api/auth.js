4a7035363fe69d531441523fa4ed680929f89739
AK Azad Milon <akazadmilon.dev@gmail.com>
2023-02-16 15:05:47 +0600

    auth registration condition

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,8 +3,21 @@ const _ = express.Router()
 
 
 _.post('/register', (req, res)=> {
-    const {email, fullname} = req.body
-    res.json({email, fullname})
+    const {email, fullname, password, isSubscribe} = req.body
+
+    if(!email){
+        return res.json({error:"you must be enter your email!"})
+    }
+
+    if(!fullname){
+        return res.json({error:"you must be enter your fullname"})
+    }
+
+    if(!password){
+        return res.json({error:"you must be enter your password"})
+    }
+    
+    res.json({email, fullname, password, isSubscribe})
 })
 
 module.exports = _;
